538159bf1b92073d641da64e8e9e8cadd147c063
ADC04 <allendcooper@gmail.com>
2020-11-18 22:36:43 -0500

    added mobile responsive video

--- src/theme/modules/components/Video.js
+++ src/theme/modules/components/Video.js
@@ -2,29 +2,37 @@ import React from "react";
 
 const Video = () => {
 
-    const style = {
-        left: "50%",
-        top: "50%",
-        width: "auto",
-        height: "auto",
-        position: "fixed",
-        transform: "translate(-50%, -50%)",
-        minWidth: "100%",
-        minHeight: "100%",
-        zIndex: -1,
-    };
+  const style = {
+    left: "50%",
+    top: "50%",
+    width: "auto",
+    height: "auto",
+    position: "fixed",
+    transform: "translate(-50%, -50%)",
+    minWidth: "100%",
+    minHeight: "100%",
+    zIndex: -1,
+  };
 
-    return (
-        <video
-            src={`${process.env.PUBLIC_URL}/assets/background_video_cropped_mirror.mp4`}
-            type={"video/mp4"}
-            autoPlay={true}
-            loop={true}
-            style={style}
-            muted={true}
-        >
-        </video>
-    );
+  const getVideoSrc = width => {
+    if (width >= 1080) return `${process.env.PUBLIC_URL}/assets/background_video_cropped_mirror.mp4`;
+    if (width >= 720) return `${process.env.PUBLIC_URL}/assets/background_video_cropped_mirror.mp4`;
+    return `${process.env.PUBLIC_URL}/assets/background_video_cropped_mirror_mobile.mp4`;
+  };
+
+  const src = getVideoSrc(window.innerWidth);
+
+  return (
+    <video
+      src={src}
+      type={"video/mp4"}
+      autoPlay={true}
+      loop={true}
+      style={style}
+      muted={true}
+    >
+    </video>
+  );
 }
 
 export default Video;
